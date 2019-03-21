7b5ada3cc16aa326e24537b6ac05bd1e16e63b84
DanielUche <dank.uche@yahoo.com>
2019-03-21 17:36:49 +0100

    updated django admin with possible indent errors i want travis to catch

--- app/core/models.py
+++ app/core/models.py
@@ -2,14 +2,14 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
     PermissionsMixin
 
-# Create your models here.
-
 
 class UserManager(BaseUserManager):
+
     def create_user(self, email, password=None, **extra_fields):
-        """Creates and saves a new user"""
+        """Creates and saves a new User"""
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError('Users must have an email address')
+
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -18,8 +18,8 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
         """Creates and saves a new super user"""
-        user = self.model(email=self.normalize_email(email))
-        user.is_Staff = True
+        user = self.create_user(email, password)
+        user.is_staff = True
         user.is_superuser = True
         user.save(using=self._db)
 
@@ -27,7 +27,7 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-    """custom user model tat supports using email instead of username"""
+    """Custom user model that supports using email instead of username"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
